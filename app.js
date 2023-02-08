c35d9edbee038528102b3213d5decf016cfed11f
yohana valdez <yohana.valdez@hotmail.com>
2023-02-08 19:52:41 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,97 @@
+// punto1
+// const ejercicio= "fin del ejercicio";
+
+//console.log(ejercicio);
+
+//punto2
+// let palabra1= "Hola Don Pepito "
+// let palabra2= "Hola Don José "
+// let palabra3= "Pasé por su casa "
+// let palabra4= "Por su casa yo pasé "
+// let palabra5= "Vio Usted a mi abuela "
+// let palabra6= "A su abuela yo la vi "
+// let palabra7= "Adios Don Pepito "
+// let palabra8= "Adios Don José "
+
+// console.log(palabra2 + palabra6 + palabra3 + palabra8 + palabra1 + palabra4 + palabra5 + palabra7);
+
+//punto3
+
+let verdura= 10.5;
+let carne= 12.5;
+let comidaperro= 20.4;
+let fruta =30.5;
+let papelhigienico=7.5;
+let yerba= 11.3;
+
+// let primerasuma= verdura+carne+comidaperro+fruta+papelhigienico+yerba;
+// console.log(primerasuma);
+
+// let descuento= comidaperro*.10;
+
+// let segundasuma= descuento+verdura+carne+fruta+papelhigienico+yerba;
+
+// let recargo= segundasuma*0.18;
+
+// let total=segundasuma+recargo;
+
+// let cuotas=total/3;
+
+// let pagomitad=segundasuma/2;
+
+// let segundorecargo=pagomitad*0.18;
+
+// let mitadcontarjeta=pagomitad+segundorecargo;
+
+// let cuotas2=mitadcontarjeta/3;
+
+// console.log(pagomitad);
+// console.log(mitadcontarjeta);
+// console.log(cuotas2);
+
+let number1=10;
+let number2=15;
+let number3=25;
+let number4=63;
+let number5=48;
+let number6=78;
+
+let mayorque=number1>=number4;
+
+console.log(mayorque);
+
+let menorque=number5<=number1;
+
+console.log(menorque);
+
+let suma= number2 > number1 && number1 < number3;
+
+console.log(suma);
+
+let suma1= number6 === number1 && number3 <= number2;
+
+console.log(suma1);
+
+let suma2= (number1 + number2 === number3) && (number6 >= number4);
+
+console.log(suma2);
+
+let suma3= (number3 >= number5 + number6) || (number5 <= number6);
+
+console.log(suma3);
+
+let suma4= suma3 || suma1;
+
+console.log(suma4);
+
+let suma5= (suma4 || suma3) || (suma2 || suma1);
+
+console.log(suma5);
+
+const num1=number1/2 && number1 >= 50;
+
+console.log(num1);
+
+const num2=number4/2 || number4 >= 8;
+
+console.log(num2);
